5d4cac5df42d3eebab56c4f627993580a5d8b9f4
Andy Galkin <dalamar132115@gmail.com>
2024-06-09 21:36:24 +0300

    Перенес создание некоторых объектов в методы

--- src/com/github/andrewcoder/HomeRunner.java
+++ src/com/github/andrewcoder/HomeRunner.java
@@ -1,25 +1,19 @@
 package com.github.andrewcoder;
 
+import java.util.Random;
+
 public class HomeRunner {
     public static void main(String[] args) {
-        Room room1 = new Room(false);
-        Room room2 = new Room(false);
-        Room room3 = new Room(true);
-        Room room4 = new Room(true);
-        Room[] rooms = {room1, room2};
-        Room[] rooms2 = {room3, room4};
-
-        Flat flat1 = new Flat(11, rooms);
-        Flat flat2 = new Flat(12, rooms2);
-        Flat flat3 = new Flat(13, rooms);
-        Flat flat4 = new Flat(14, rooms2);
-        Flat[] flats1 = new Flat[]{flat1, flat2};
-        Flat[] flats2 = new Flat[]{flat3, flat4};
-
+        Room[] rooms = makeRooms(4);
+        Room[] rooms2 = makeRooms(5);
+        Room[] rooms3 = makeRooms(3);
+        Flat[] flats1 = makeFlats(1,rooms);
+        Flat[] flats2 = makeFlats(3,rooms3);
+        Flat[] flats3 = makeFlats(2,rooms2);
         Floor floor1 = new Floor(1, flats1);
         Floor floor2 = new Floor(2, flats2);
         Floor floor3 = new Floor(3, flats1);
-        Floor floor4 = new Floor(4, flats2);
+        Floor floor4 = new Floor(4, flats3);
         Floor[] floors = new Floor[]{floor1, floor2, floor3, floor4};
 
         Home home = new Home(10,floors);
@@ -36,5 +30,29 @@ public class HomeRunner {
                 }
             }
         }
+    }
+    public static Room[] makeRooms(int countRoom) {
+        Room[] rooms = new Room[countRoom];
+        for (int i = 0; i < rooms.length; i++) {
+            int random = new Random().nextInt(2, 4);
+            if (i % random == 0) {
+                rooms[i] = new Room(true);
+            } else {
+                rooms[i] = new Room(false);
+            }
+        }
+        return rooms;
+    }
+    public static Flat[] makeFlats(int countFlats, Room[] rooms) {
+        Flat[] flats = new Flat[countFlats];
+        for (int i = 0; i < flats.length; i++) {
+            int random = new Random().nextInt(2, 4);
+            if (i % random == 0) {
+                flats[i] = new Flat(i + 1, rooms);
+            } else {
+                flats[i] = new Flat(i + 1, rooms);
+            }
+        }
+        return flats;
     }
 }
